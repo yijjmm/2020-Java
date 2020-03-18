11cd0570b8dec008c6deddbe30e00125aa0d7e32
Jimin Lee <47144126+yijjmm@users.noreply.github.com>
2020-03-18 22:55:49 +0900

    Create 200318 print.java

---
+++ 200318 print.java
@@ -0,0 +1,19 @@
+// 자바 프로그램 작성 = 클래스를 작성
+// 자바 : 객체지향 언어
+class Hello {
+	public static void main(String [] args){
+		System.out.printf("Hello, world!");
+		// 서식지정자 (%s, %c, %d) 사용 가능
+		System.out.println("이지민");
+		// 개행 추가된 print() 함수
+		System.out.printf("2019113318");
+		// 서식지정자 사용불가
+		
+		// 모니터 = 표준출력 = System.out
+		
+	}
+}
+
+// console 창에 내릴 명령
+// javac CLASS.java
+// java CLASS
